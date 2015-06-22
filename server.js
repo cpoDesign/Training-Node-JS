8b2e0b2aa41c2a265274fb36c7e12bc0d1d2af49
Pavel <pavel.svarc@cpodesign.com>
2015-06-22 21:43:00 +0100

    Updated server.js configuration to add some new comments

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ var http = require('http');
 var express = require('express');
 var _ = require('underscore');
 var app = express();
-var ejsEngine = require('ejs-locals');
 var url = require('url');
 var controllers = require('./controllers');
 
 
-// Setup view engine
-app.set('view engine', 'vash'); // ejs view engine
+// Setup view engine // vash view engine
+app.set('view engine', 'vash');
+// alternative view engine
+// var ejsEngine = require('ejs-locals');
 
-// map controllers
+// map controllers on server side
 controllers.init(app);
 
 // allow access to public directory
@@ -19,6 +20,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(function (req, res, next) {
     console.log('%s %s', req.method, req.url);
+    // node way of passing call to next method. can be at any point returned as result
     next();
 });
 
@@ -39,20 +41,22 @@ app.use(function (req, res, next) {
 });
 
 
-//
+// parse path to this action
 app.use('/loadTeamEvents/:teamId/:year/:month', function (request, response) {
 
-
+    // getting information from request defined in : '/loadTeamEvents/:teamId/:year/:month'
     var teamId = request.params.teamId,
         year = request.params.year,
         month = request.params.month;
 
+    // create data model
     var data = {
         title: 'Year:' + year + ' Month:' + month,
         teamId: teamId,
         content: ''
-    }
+    };
 
+    // for each 1 to 5 create content message with events for team
         for (var i = 0; i < 5; i++) {
             data.content += '<li> Team' + data.teamId + ' Event' + i + '</li>';
         }
@@ -61,7 +65,7 @@ app.use('/loadTeamEvents/:teamId/:year/:month', function (request, response) {
 
 });
 
-
+// lets attemt to make call to server
 app.get('/api/sql', function(request, response){
    var msnodesql = require('msnodesql');
     var connString="";
